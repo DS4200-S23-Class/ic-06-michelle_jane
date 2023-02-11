41173fbc61fab2e8d52aa4eada3d506617be6f1c
Michelle Wang <michellewang0402@gmail.com>
2023-02-11 16:30:23 -0500

    in progress

--- main.js
+++ main.js
@@ -1,25 +1,25 @@
+// event handler for when button is clicked
 function buttonClicked(){
 	let bar = document.getElementById("line");
 	bar.classList.add("red");
 }
-
 // add event handler to button
 document.getElementById("changeButton").addEventListener("click", buttonClicked);
 
 
+// event handler for when the pointer mouses over the circle shape
 function mouseOver(){
 	let shape = document.getElementById("shape");
 	shape.classList.add("orange");
 }
-
 // add event handler to button
 document.getElementById("shape").addEventListener("mouseover", mouseOver);
 
 
+// event handler for when theh mounter leaves (mouses out) of the circle shape
 function mouseOut(){
 	let shape = document.getElementById("shape");
 	shape.classList.remove("orange");
 }
-
 // add event handler to button
 document.getElementById("shape").addEventListener("mouseout", mouseOut);
